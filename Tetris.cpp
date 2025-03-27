da293933d8a60f177361d9093cf858688d97a960
Saumya2721 <saumyatinna@gmail.com>
2025-03-27 11:43:30 +0530

    Game Code

--- Tetris.cpp
+++ Tetris.cpp
@@ -15,13 +15,20 @@ const string EMPTY = "  ";
 // Tetromino shapes and colors
 const string COLORS[7] = {"🟦", "🟨", "🟪", "🟩", "🟥", "🟧", "⬜"};
 const int tetrominoes[7][4][4] = {
-    {{1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, // I
-    {{1, 1, 0, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, // O
-    {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, // T
-    {{0, 1, 1, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, // S
-    {{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, // Z
-    {{1, 0, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}, // J
-    {{0, 0, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}  // L
+    // I
+    {{1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+    // O
+    {{1, 1, 0, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+    // T
+    {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+    // S
+    {{0, 1, 1, 0}, {1, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+    // Z
+    {{1, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+    // J
+    {{1, 0, 0, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+    // L
+    {{0, 0, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}}
 };
 
 class Tetromino {
@@ -43,7 +50,6 @@ public:
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
                 temp[j][3 - i] = shape[i][j];
-
         for (int i = 0; i < 4; i++)
             for (int j = 0; j < 4; j++)
                 shape[i][j] = temp[i][j];
@@ -69,21 +75,23 @@ public:
             cout << "|";
             for (int j = 0; j < WIDTH; j++) {
                 string displayChar = grid[i][j];
-
-                for (int ti = 0; ti < 4; ti++)
-                    for (int tj = 0; tj < 4; tj++)
+                // Overlay current Tetromino if it's within this cell
+                for (int ti = 0; ti < 4; ti++) {
+                    for (int tj = 0; tj < 4; tj++) {
                         if (t.shape[ti][tj] && (i == t.y + ti) && (j == t.x + tj))
                             displayChar = t.color;
-
+                    }
+                }
                 cout << displayChar;
             }
             cout << "|\n";
         }
         cout << " ";
         for (int i = 0; i < WIDTH; i++) cout << "--";
-        cout << " \n";
+        cout << "\n";
         cout << "Score: " << score << "  Level: " << level << "  Lines: " << linesCleared << "\n";
-        cout << "[A] Left  [D] Right  [S] Down  [W] Rotate  [Space] Hard Drop  [P] Pause  [R] Restart  [X] Exit\n";
+        cout << "[A] Left  [D] Right  [S] Down  [W] Rotate  [Space] Hard Drop\n";
+        cout << "[P] Pause  [R] Restart  [X] Exit\n";
     }
 
     bool canMove(Tetromino &t, int dx, int dy) {
@@ -111,21 +119,34 @@ public:
         for (int i = 0; i < HEIGHT; i++) {
             bool full = true;
             for (int j = 0; j < WIDTH; j++)
-                if (grid[i][j] == EMPTY) full = false;
-
+                if (grid[i][j] == EMPTY) {
+                    full = false;
+                    break;
+                }
             if (full) {
                 fullLines++;
+                // Shift all rows above downward
                 for (int k = i; k > 0; k--)
                     for (int j = 0; j < WIDTH; j++)
                         grid[k][j] = grid[k - 1][j];
-
+                // Clear top row
                 for (int j = 0; j < WIDTH; j++)
                     grid[0][j] = EMPTY;
             }
         }
+        // Scoring: 1 line = 100, 2 lines = 300, 3 lines = 500, 4 lines = 800
         score += fullLines == 1 ? 100 : fullLines == 2 ? 300 : fullLines == 3 ? 500 : fullLines == 4 ? 800 : 0;
         linesCleared += fullLines;
         if (linesCleared >= level * 10) level++;
+    }
+
+    void clearBoard() {
+        for (int i = 0; i < HEIGHT; i++)
+            for (int j = 0; j < WIDTH; j++)
+                grid[i][j] = EMPTY;
+        score = 0;
+        level = 1;
+        linesCleared = 0;
     }
 };
 
@@ -133,14 +154,76 @@ class TetrisGame {
 public:
     GameBoard board;
     Tetromino *currentPiece;
+    bool gameOver;
+    bool isPaused;
 
-    TetrisGame() { currentPiece = new Tetromino(rand() % 7); }
+    TetrisGame() : gameOver(false), isPaused(false) {
+        srand(time(0));
+        currentPiece = new Tetromino(rand() % 7);
+    }
+
+    ~TetrisGame() {
+        delete currentPiece;
+    }
 
     void run() {
-        while (true) {
+        while (!gameOver) {
+            handleInput();
+            if (!isPaused) {
+                update();
+            }
             board.draw(*currentPiece);
+            // Adjust speed based on level; lower sleep time for higher levels
             usleep(500000 / board.level);
-            update();
+        }
+        cout << "Game Over!" << endl;
+    }
+
+private:
+    char getInput() {
+        struct termios oldt, newt;
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+        int bytesAvailable;
+        ioctl(STDIN_FILENO, FIONREAD, &bytesAvailable);
+        char key = 0;
+        if (bytesAvailable > 0) {
+            read(STDIN_FILENO, &key, 1);
+        }
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        return key;
+    }
+
+    void handleInput() {
+        char key = getInput();
+        if (key == 'p' || key == 'P') {
+            isPaused = !isPaused;
+            if (isPaused) {
+                cout << "Game Paused. Press 'p' to resume...\n";
+            }
+        }
+        if (isPaused) return;
+        if (key == 'a' || key == 'A') {
+            if (board.canMove(*currentPiece, -1, 0))
+                currentPiece->x--;
+        } else if (key == 'd' || key == 'D') {
+            if (board.canMove(*currentPiece, 1, 0))
+                currentPiece->x++;
+        } else if (key == 's' || key == 'S') {
+            if (board.canMove(*currentPiece, 0, 1))
+                currentPiece->y++;
+        } else if (key == 'w' || key == 'W') {
+            currentPiece->rotate();
+            // Optional: Check if rotation results in collision; if so, undo.
+        } else if (key == ' ') {
+            hardDrop();
+        } else if (key == 'r' || key == 'R') {
+            restartGame();
+        } else if (key == 'x' || key == 'X') {
+            exit(0);
         }
     }
 
@@ -148,15 +231,34 @@ public:
         if (!board.canMove(*currentPiece, 0, 1)) {
             board.placeTetromino(*currentPiece);
             delete currentPiece;
+            // If the new piece immediately collides, game over.
             currentPiece = new Tetromino(rand() % 7);
+            if (!board.canMove(*currentPiece, 0, 0)) {
+                gameOver = true;
+            }
         } else {
             currentPiece->y++;
         }
     }
+
+    void hardDrop() {
+        while (board.canMove(*currentPiece, 0, 1)) {
+            currentPiece->y++;
+            board.score++; // Add a bonus point per row dropped (optional)
+        }
+        update();
+    }
+
+    void restartGame() {
+        board.clearBoard();
+        delete currentPiece;
+        currentPiece = new Tetromino(rand() % 7);
+        gameOver = false;
+        isPaused = false;
+    }
 };
 
 int main() {
-    srand(time(0));
     TetrisGame game;
     game.run();
     return 0;
